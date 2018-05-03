2ea4aad10715b2ea87b1b40dda6e1237f80f89d0
Roberto Nogueira <enogrob@gmail.com>
2018-05-03 04:11:23 -0300

    request-params-and-status-codes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,7 +16,7 @@ defmodule Servy.Handler do
     |> List.first
     |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
   def route(conv) do
@@ -24,26 +24,82 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-     %{conv | resp_body: "Bears, Lions, Tigers"}
+     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-     %{conv | resp_body: "Teddy, Smokey, Paddington"}
+     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+     %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+     %{conv | status: 403, resp_body: "Deleting a bear is Forbidden!"}
+  end
+
+  def route(conv, _method, path) do
+     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    codes = %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "internal Server Error"
+    }
+    codes[code]
+  end
 end
 
 request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request = """
+GET /teddy HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
