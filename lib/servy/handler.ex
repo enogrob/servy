9b52e7a25f14241a47c19a5db0a4e3a4acc4927f
Roberto Nogueira <enogrob@gmail.com>
2018-05-07 11:05:23 -0300

    rewrite-paths-and-track-404s-exercises

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,3 +1,5 @@
+require Logger
+
 defmodule Servy.Handler do
   def handle(request) do
     request
@@ -5,57 +7,69 @@ defmodule Servy.Handler do
     |> rewrite_path
     |> log
     |> route
+    |> emojify
     |> track
     |> format_response
   end
 
+def emojify(%{status: 200} = conv) do
+  emojies = String.duplicate("🎉", 5)
+  body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+  %{ conv | resp_body: body }
+end
+
+def emojify(conv), do: conv
+
   def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning #{path} is on the loose"
+    Logger.error "Warning #{path} is on the loose"
     conv
   end
 
   def track(conv), do: conv
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-     %{conv | path: "/wildthings"}
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
-
   def rewrite_path(conv), do: conv
 
-  def log(conv), do: IO.inspect conv
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+  def rewrite_path_captures(conv, nil), do: conv
+
+  def log(conv), do: IO.inspect(conv)
 
   def parse(request) do
     [method, path, _] =
-    request
-    |> String.split("\n")
-    |> List.first
-    |> String.split(" ")
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
 
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  # def route(conv) do
-  #    route(conv, conv.method, conv.path)
-  # end
-
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
-     %{conv | status: 200, resp_body: "Bear #{id}"}
+    %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
   def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
-     %{conv | status: 403, resp_body: "Deleting a bear is Forbidden!"}
+    %{conv | status: 403, resp_body: "Deleting a bear is Forbidden!"}
   end
 
   def route(%{path: path} = conv) do
-     %{conv | status: 404, resp_body: "No #{path} here!"}
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
@@ -77,6 +91,7 @@ defmodule Servy.Handler do
       404 => "Not Found",
       500 => "internal Server Error"
     }
+
     codes[code]
   end
 end
@@ -139,4 +154,4 @@ Bears, Lions, Tigers
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
